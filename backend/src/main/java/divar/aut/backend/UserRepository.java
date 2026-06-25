32d3a299adb3243eeb4936532186ddb19e32481d
Alireza Bolhasani <Alirezabo957@gmail.com>
2026-06-26 03:24:37 +0330

    feat(backend): add User entity and configure SQLite database connection

--- backend/src/main/java/divar/aut/backend/UserRepository.java
+++ backend/src/main/java/divar/aut/backend/UserRepository.java
@@ -1,35 +1,35 @@
-package divar.aut.backend;
-
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class UserRepository {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    public UserRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.jdbcTemplate.execute(
-                "CREATE TABLE IF NOT EXISTS users (" +
-                        "username TEXT PRIMARY KEY, " +
-                        "password TEXT)"
-        );
-    }
-
-    public boolean saveUser(String username, String password) {
-        try {
-            String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-            jdbcTemplate.update(sql, username, password);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public boolean verifyUser(String username, String password) {
-        String sql = "SELECT COUNT(*) FROM users WHERE username = ? AND password = ?";
-        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, username, password);
-        return count != null && count > 0;
-    }
-}
+//package divar.aut.backend;
+//
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public class UserRepository {
+//
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    public UserRepository(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//        this.jdbcTemplate.execute(
+//                "CREATE TABLE IF NOT EXISTS users (" +
+//                        "username TEXT PRIMARY KEY, " +
+//                        "password TEXT)"
+//        );
+//    }
+//
+//    public boolean saveUser(String username, String password) {
+//        try {
+//            String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+//            jdbcTemplate.update(sql, username, password);
+//            return true;
+//        } catch (Exception e) {
+//            return false;
+//        }
+//    }
+//
+//    public boolean verifyUser(String username, String password) {
+//        String sql = "SELECT COUNT(*) FROM users WHERE username = ? AND password = ?";
+//        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, username, password);
+//        return count != null && count > 0;
+//    }
+//}
