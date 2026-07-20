51bfcb29baa9e69dd3e4e9914d4a800eb10f49a6
Alireza Bolhasani <alirezabolhasani130@gmail.com>
2026-07-20 16:03:22 +0330

    feat: javadoc is added to CategoryResponse

--- backend/src/main/java/divar/aut/backend/dto/CategoryResponse.java
+++ backend/src/main/java/divar/aut/backend/dto/CategoryResponse.java
@@ -2,10 +2,28 @@ package divar.aut.backend.dto;
 
 import divar.aut.backend.entity.Category;
 
+/**
+ * Data Transfer Object for category responses.
+ * <p>
+ * Contains the ID and name of a category, built from a {@link Category} entity.
+ * </p>
+ */
 public class CategoryResponse {
+    /**
+     * The unique identifier of the category.
+     */
     private Long id;
+
+    /**
+     * The name of the category.
+     */
     private String name;
 
+    /**
+     * Constructs a CategoryResponse from a Category entity.
+     *
+     * @param category the category entity.
+     */
     public CategoryResponse(Category category) {
         this.id = category.getId();
         this.name = category.getName();
